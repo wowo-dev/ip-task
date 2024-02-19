7643d05b8bead8c7560bb00d472bd6de446cfd10
Wojciech Woźniak <wojtek.wozniak.dev@gmail.com>
2024-02-19 16:19:29 +0100

    tests(`PackListController`): Error state snapshot.

--- InPost Interview Coding TaskTests/Snapshots/PackListControllerTests.swift
+++ InPost Interview Coding TaskTests/Snapshots/PackListControllerTests.swift
@@ -23,10 +23,24 @@ final class PackListControllerTest: XCTestCase {
     func test_packList_emptyState() async throws {
         networkingMock.getPacksPackReturnValue = []
 
+        let vc = try await createVC()
+        assertSnapshot(of: vc, as: .image, record: false)
+    }
+
+    func test_packList_errorState() async throws {
+        networkingMock.getPacksPackThrowableError = NSError(domain: "", code: 0)
+
+        let vc = try await createVC()
+        assertSnapshot(of: vc, as: .image, record: false)
+    }
+
+    // MARK: - Helpers
+
+    func createVC() async throws -> UIViewController {
         let vc = PackListController()
         let nc = IPNavigationController(rootViewController: vc)
         _ = nc.view.snapshotView(afterScreenUpdates: true)
         try await Task.sleep(for: .seconds(0.1))
-        assertSnapshot(of: nc, as: .image, record: false)
+        return nc
     }
 }
